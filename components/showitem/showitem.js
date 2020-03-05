32421c759f72566af2817729489c38d15d0862c1
Zhuo <1787288097@qq.com>
2020-03-05 18:14:13 +0800

    组件化、增加字段、数据存为JSON
    1. 将新闻页的新闻项提取成一个组件
    2. 删除轮播图官方文档的多余代码（事件函数等）
    3. 将原data里的新闻内容单独存为一个json文件，用于调试请求
    4. 增加新闻内容的字段（id，time，url等）

---
+++ components/showitem/showitem.js
@@ -0,0 +1,34 @@
+// components/showItem/showItem.js
+Component({
+  options: {
+    multipleSlots: true
+  },
+
+  /**
+   * 组件的属性列表
+   */
+  properties: {
+    itemSrc: {
+      type: String,
+      value: ''
+    },
+    itemUrl: {
+      type: String,
+      value: ''
+    }
+  },
+
+  /**
+   * 组件的初始数据
+   */
+  data: {
+
+  },
+
+  /**
+   * 组件的方法列表
+   */
+  methods: {
+
+  }
+})
--- pages/index/index.js
+++ pages/index/index.js
@@ -3,48 +3,14 @@
 const app = getApp()
 
 Page({
-
-
-
   data: {
-    contactList: [{
-      "name": "标题一",
-      "details": "由各种物质组成的矩形球状天梯，叫做星球。",
-    }, {
-        "name": "标题二",
-        "details": "由各种物质组成的矩形球状天梯，叫做星球。",
-    }, {
-        "name": "标题三",
-        "details": "由各种物质组成的矩形球状天梯，叫做星球。",
-    }, {
-        "name": "标题四",
-        "details": "由各种物质组成的矩形球状天梯，叫做星球。",
-    }, {
-        "name": "标题五",
-        "details": "由各种物质组成的矩形球状天梯，叫做星球。",
-    }, {
-        "name": "标题六",
-        "details": "由各种物质组成的矩形球状天梯，叫做星球。",
-    }, {
-        "name": "标题七",
-        "details": "由各种物质组成的矩形球状天梯，叫做星球。",
-    }, {
-        "name": "标题八",
-        "details": "由各种物质组成的矩形球状天梯，叫做星球。",
-    }, {
-        "name": "标题九",
-        "details": "由各种物质组成的矩形球状天梯，叫做星球。",
-    }]  ,
+    contactList: [],
     
     imgUrls: [
       '/images/minzu1.jpg',
       '/images/minzu2.jpg',
       '/images/minzu3.jpg'
-    ],
-    indicatorDots: false,
-    autoplay: false,
-    interval: 2000,
-    duration: 1000
+    ]
   },
 
   //点击切换隐藏和显示
@@ -61,26 +27,5 @@ Page({
         uhide: itemId
       })
     }
-  },
-
-  changeIndicatorDots(e) {
-    this.setData({
-      indicatorDots: !this.data.indicatorDots
-    })
-  },
-  changeAutoplay(e) {
-    this.setData({
-      autoplay: !this.data.autoplay
-    })
-  },
-  intervalChange(e) {
-    this.setData({
-      interval: e.detail.value
-    })
-  },
-  durationChange(e) {
-    this.setData({
-      duration: e.detail.value
-    })
   }
 })
